1b406d0cac551e69a7b3e25c117849770e4bf608
Temmuz Güzel <todguzel@gmail.com>
2020-05-20 16:57:38 +0200

    Initial Commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Temmuz Guzel on 20.05.20.
+//  Copyright © 2020 Temmuz Guzel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
